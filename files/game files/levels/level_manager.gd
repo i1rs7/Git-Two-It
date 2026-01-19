a95df79b4b9ede046e8f08d9682bfc4f9dcf5cd1
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2026-01-19 16:54:20 -0500

    added split/merge ui

--- files/game files/levels/level_manager.gd
+++ files/game files/levels/level_manager.gd
@@ -15,7 +15,7 @@ func _process(_delta: float) -> void:
 
 func load_next_level():
 	assert(levels.size() > current_level, "next level not found - update var num_levels and ensure all level scenes are named and stored correctly")
-	if $"../UI".visible:
+	if $"../Settings".visible:
 		Input.action_press("pause")
 		Input.action_release("pause")
 	get_child(0).queue_free()
