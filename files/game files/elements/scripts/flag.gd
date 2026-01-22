ce80dd6cbe2d55cf7119e4d9645ab80ae9298f95
i1rs7 <163767002+i1rs7@users.noreply.github.com>
2026-01-21 23:45:00 -0500

    fixing flag and making groups

--- files/game files/elements/scripts/flag.gd
+++ files/game files/elements/scripts/flag.gd
@@ -3,8 +3,14 @@ extends StaticBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.play()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("octocat"):
+		get_tree().root.get_child(0).find_child("Level Manager").load_next_level()
+		await tree_exited
